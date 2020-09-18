c85be983f1ec2c4510953d7769f790b59cc91e54
Benjamin Lund <lund.b92@gmail.com>
2020-09-18 21:05:26 +0200

    Added account creation endpoint and account service to handle the request

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Server.Models;
+using Server.Services;
+
+
+namespace Server.Controllers
+{
+
+    [Route("api/v1/")]
+    [Authorize]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+
+        private readonly AccountService _accountService;
+
+        public AccountController(AccountService accountService)
+        {
+            _accountService = accountService;
+        }
+
+
+
+        /// <summary>
+        /// API endpoint which accepts a post request to register a new account
+        /// </summary>
+        [AllowAnonymous]
+        [HttpPost("account")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status409Conflict)]
+        public async Task<ActionResult<Account>> Create([FromBody] Account account)
+        {
+            if(await _accountService.Exists(account) == true)
+            {
+                var response = await _accountService.Create(account, Request.Headers["origin"]);
+                return StatusCode(200, response);
+            }
+            
+            return StatusCode(409, $"'{account.Email}' already exists.");
+        }
+
+    }
+
+}
---
+++ Services/AccountService.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Threading.Tasks;
+using MongoDB.Driver;
+using Server.Models;
+using Server.Settings;
+
+namespace Server.Services
+{
+    public class AccountService
+    {
+        private readonly IMongoCollection<Account> _accounts;
+        private readonly IEmailService _emailService;
+
+        public AccountService(DatabaseSettings settings, IEmailService emailService)
+        {
+            var client = new MongoClient(settings.ConnectionString);
+            var database = client.GetDatabase(settings.DatabaseName);
+            _accounts = database.GetCollection<Account>(settings.AccountsCollectionName);
+
+            _emailService = emailService;
+        }
+
+
+        /// <summary>
+        /// Method to check if an account exists
+        /// </summary>
+        public async Task<bool> Exists(Account account)
+        {
+            if (await _accounts.Find(x => x.Email == account.Email).FirstOrDefaultAsync() != null)
+            {
+                return true;
+            }
+            return false;
+        }
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public async Task<Account> Create(Account account, string origin)
+        {
+            account.Created = DateTime.Now;
+            account.Role = Role.User;
+            account.Password = BCrypt.Net.BCrypt.HashPassword(account.Password);
+            account.TermsAccepted = true;
+
+            using var rngCryptoServiceProvider = new RNGCryptoServiceProvider();
+            var randomBytes = new byte[40];
+            rngCryptoServiceProvider.GetBytes(randomBytes);
+            account.VerificationToken = BitConverter.ToString(randomBytes).Replace("-", ""); 
+
+            await _accounts.InsertOneAsync(account);
+
+            string verifyUrl = $"{origin}/account/verify?token={account.VerificationToken}";
+            string message = $@"
+                                <h1>Hello!</h1>
+                                <p>Please click the link below to verify your newly created account</p>
+                                <a href=""{verifyUrl}"">Verify Me</a>
+                             ";
+
+            await _emailService.Send(account.Email, "Sale account verification", message);
+
+            return account;
+        }
+
+
+    }
+}
