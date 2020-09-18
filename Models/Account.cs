7b45f9b4b9075f0e8027db499268d795fddf9406
Benjamin Lund <lund.b92@gmail.com>
2020-09-18 18:36:49 +0200

    Added initial account model

---
+++ Models/Account.cs
@@ -0,0 +1,43 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Server.Models
+{
+    public enum Role
+    {
+        Admin,
+        User
+    }
+
+    public class Account
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        [EnumDataType(typeof(Role))]
+        public Role Role { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+        public bool EmailConfirmed { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        public DateTime Created { get; set; }
+        public string VerificationToken { get; set; }
+        public bool TermsAccepted { get; set; }
+
+    }
+}
