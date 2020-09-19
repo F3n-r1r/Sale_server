b73964071dec829a640edbfc218bdabfa05b576a
Benjamin Lund <lund.b92@gmail.com>
2020-09-19 21:18:40 +0200

    Added entities folder with models for account, refreshtoken and role

---
+++ Entities/Account.cs
@@ -0,0 +1,45 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Server.Entities
+{
+    /// <summary>
+    /// The account entity model represents the data for an account in the application
+    /// </summary>
+    public class Account
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+        public string Title { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+        public bool AcceptTerms { get; set; }
+        [EnumDataType(typeof(Role))]
+        public Role Role { get; set; }
+        public string VerificationToken { get; set; }
+        public DateTime? Verified { get; set; }
+        public bool IsVerified => Verified.HasValue || PasswordReset.HasValue;
+        public string ResetToken { get; set; }
+        public DateTime? ResetTokenExpires { get; set; }
+        public DateTime? PasswordReset { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime? Updated { get; set; }
+        public List<RefreshToken> RefreshTokens { get; set; }
+
+        public bool OwnsToken(string token)
+        {
+            return this.RefreshTokens?.Find(x => x.Token == token) != null;
+        }
+
+    }
+}
---
+++ Entities/Role.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Server.Entities
+{
+    /// <summary>
+    /// The role enum defines all the available roles in the API
+    /// </summary>
+    public enum Role
+    {
+        Admin,
+        User
+    }
+}
