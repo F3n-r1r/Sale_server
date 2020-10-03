22bfab310c830936d9e103bc1f71352363336f2b
Benjamin Lund <lund.b92@gmail.com>
2020-10-03 13:38:05 +0200

    Added base controller with account property that returns currently authenticated account

---
+++ Controllers/BaseController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Server.Entities;
+
+namespace Server.Controllers
+{
+    /// <summary>
+    /// The base controller is inherited by all other controllers in the boilerplate api and includes common properties and methods that are accessible to all controllers.
+    /// The Account property returns the current authenticated account for the request from the HttpContext.Items collection, or returns null if the request is not authenticated.
+    /// The current account is added to the HttpContext.Items collection by the custom jwt middleware when the request contains a valid JWT token in the authorization header.
+    /// </summary>
+    [Controller]
+    public abstract class BaseController : ControllerBase
+    {
+        public Account Account => (Account)HttpContext.Items["Account"];
+    }
+}
