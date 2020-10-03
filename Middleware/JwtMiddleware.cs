91565b96c40cd88eade0521b4b37d5524beb1867
Benjamin Lund <lund.b92@gmail.com>
2020-10-03 13:18:35 +0200

    Added jwt middleware to validate token and attach authenticated account to the http context

---
+++ Middleware/JwtMiddleware.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Server.Entities;
+using MongoDB.Driver;
+using Server.Settings;
+
+namespace Server.Middleware
+{
+    public class JwtMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly IMongoCollection<Account> _accounts;
+        private readonly AuthSettings _authSettings;
+
+        public JwtMiddleware(RequestDelegate next, DatabaseSettings databaseSettings, IOptions<AuthSettings> authSettings)
+        {
+            _next = next;
+            _authSettings = authSettings.Value;
+
+            var client = new MongoClient(databaseSettings.ConnectionString);
+            var database = client.GetDatabase(databaseSettings.DatabaseName);
+            _accounts = database.GetCollection<Account>(databaseSettings.AccountsCollectionName);
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            var token = context.Request.Headers["Authorization"].FirstOrDefault()?.Split(" ").Last();
+
+            if (token != null)
+                await attachAccountToContext(context, token);
+
+            await _next(context);
+        }
+
+        private async Task attachAccountToContext(HttpContext context, string token)
+        {
+            try
+            {
+                var tokenHandler = new JwtSecurityTokenHandler();
+                var key = Encoding.ASCII.GetBytes(_authSettings.Secret);
+                tokenHandler.ValidateToken(token, new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(key),
+                    ValidateIssuer = false,
+                    ValidateAudience = false,
+                    // set clockskew to zero so tokens expire exactly at token expiration time (instead of 5 minutes later)
+                    ClockSkew = TimeSpan.Zero
+                }, out SecurityToken validatedToken);
+
+                var jwtToken = (JwtSecurityToken)validatedToken;
+                var accountId = jwtToken.Claims.First(x => x.Type == "id").Value;
+
+                // attach account to context on successful jwt validation
+                context.Items["Account"] = await _accounts.Find(x => x.Id == accountId).FirstOrDefaultAsync();
+            }
+            catch
+            {
+                // do nothing if jwt validation fails
+                // account is not attached to context so request won't have access to secure routes
+            }
+        }
+    }
+}
